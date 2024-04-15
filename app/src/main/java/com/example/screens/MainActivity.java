d3f10ee7fc12380b084feec78e1c0752af260280
a23jonam <a23jonam@student.his.se>
2024-04-15 16:48:51 +0200

    added an extra intent in SecondActivity

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("name", "Jonas");
                 startActivity(intent);
             }
         });
